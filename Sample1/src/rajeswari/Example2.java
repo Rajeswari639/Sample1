4d8ad83b6589202d6837b9f86221a6dbdc926ad6
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-26 22:40:51 +0530

    commit ok

---
+++ Sample1/src/rajeswari/Example2.java
@@ -0,0 +1,17 @@
+package rajeswari;
+
+public class Example2 {
+ public static void main(String[] arg) {
+	if(arg.length==0) {
+		System.out.println("no value");
+	}
+	else {
+		int i;
+		for(i=0;i<arg.length;i++)
+		{
+			System.out.println(arg[i]+",");
+		}
+	}
+}	
+
+}
