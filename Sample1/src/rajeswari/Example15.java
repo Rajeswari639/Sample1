6483bebf5894bf98570f27c14ef2fc8aead87b2d
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-27 22:02:25 +0530

    commit ok

---
+++ Sample1/src/rajeswari/Example15.java
@@ -0,0 +1,51 @@
+package rajeswari;
+
+public class Example15 {
+	public static void main(String[] args) {
+		int month=Integer.parseInt(args[0]);
+		switch(month)
+		{
+		case 1:
+			System.out.println("january");
+			break;
+		case 2:
+			System.out.println("february");
+			break;
+		case 3:
+			System.out.println("march");
+			break;
+		case 4:
+			System.out.println("April");
+			break;
+		case 5:
+			System.out.println("may");
+			break;
+		case 6:
+			System.out.println("June");
+			break;
+		case 7:
+			System.out.println("July");
+			break;
+		case 8:
+			System.out.println("August");
+			break;
+		case 9:
+			System.out.println("september");
+			break;
+		case 10:
+			System.out.println("october");
+			break;
+		case 11:
+			System.out.println("November");
+			break;
+		case 12:
+			System.out.println("December");
+			break;
+		case 13:
+			System.out.println("Invalid code");
+			break;
+		}
+			
+	}
+
+}
