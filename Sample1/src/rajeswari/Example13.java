3b1cb68edd48f4bd8df63cdc3931679007751ff0
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-27 20:55:42 +0530

    commit ok

---
+++ Sample1/src/rajeswari/Example13.java
@@ -0,0 +1,13 @@
+package rajeswari;
+
+public class Example13 {
+	public static void main(String[] args) {
+		int j=0;
+		for(j=23;j<=57;j++)
+		{
+			if(j%2==0)
+				System.out.println(j);
+		}
+	}
+
+}
