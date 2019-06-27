2292077d46565cbcb6585cae443e3ba614951490
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-27 15:33:20 +0530

    commit ok

---
+++ Sample1/src/rajeswari/Example6.java
@@ -0,0 +1,10 @@
+package rajeswari;
+
+public class Example6 {
+	public static void main(String[] args) {
+		int s=Integer.parseInt(args[0]);
+		int t=Integer.parseInt(args[1]);
+		System.out.println("the total of"+s+"and "+t+" is " +(s+t));
+	}
+
+}
