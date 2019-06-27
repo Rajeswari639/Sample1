7aa00515c47947de27304057d93be15f0a58cf83
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-27 10:20:35 +0530

    commit ok

--- Sample1/src/rajeswari/Example4.java
+++ Sample1/src/rajeswari/Example4.java
@@ -2,12 +2,13 @@ package rajeswari;
 
 public class Example4 {
 	public static void main(String[] args) {
-		char a=args[0].charAt(0);
-		char b=args[1].charAt(1);
-		if(a>b)
+		char t=args[0].charAt(0);
+		char u=args[1].charAt(1);
+		if(t>u)
 			System.out.println(args[1]+","+args[0]);
 		else
 			System.out.println(args[0]+","+args[1]);
 	}
+
 
 }
