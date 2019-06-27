fac2d2e449c1153c04c1069738d87506d996eb89
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-27 22:21:17 +0530

    commit ok

---
+++ Sample1/src/rajeswari/Example18.java
@@ -0,0 +1,16 @@
+package rajeswari;
+
+public class Example18 {
+	public static void main(String[] args) {
+		int i=0,j=0,n=Integer.parseInt(args[0]);
+		for(i=0;i<n;i++)
+		{
+			for(j=0;j<=i;j++)
+			{
+				System.out.println(" *");
+			}
+			System.out.println("\n");
+		}
+	}
+
+}
