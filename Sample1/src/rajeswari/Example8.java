76fec49d3d838805b4c5f233dec5919f5a2cce16
mylaptop <mylaptop@DESKTOP-2QOB21C>
2019-06-27 15:45:17 +0530

    ok

---
+++ Sample1/src/rajeswari/Example8.java
@@ -0,0 +1,12 @@
+package rajeswari;
+
+public class Example8 {
+	public static void main(String[] args) {
+		int n=Integer.parseInt(args[0]);
+		if(n%2==0)
+			System.out.println("even");
+		else
+			System.out.println("odd");
+	}
+
+}
